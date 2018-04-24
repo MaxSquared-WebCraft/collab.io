d3ae69c186c2468a239c53f6f5b56ad0db3a1f1b
Max Mayr <max.mayr@gmail.com>
2018-04-24 21:22:45 +0200

    changed default colors, removed console logs

--- src/app/drawing-surface/drawing-surface-tool/drawing-surface-tool.component.ts
+++ src/app/drawing-surface/drawing-surface-tool/drawing-surface-tool.component.ts
@@ -19,10 +19,10 @@ export class DrawingSurfaceToolComponent implements OnInit {
   ngOnInit() {
     this.arrayColors[0] = '#000000';
     this.arrayColors[1] = '#2883e9';
-    this.arrayColors[2] = '#e920e9';
-    this.arrayColors[3] = '#FF0000';
-    this.arrayColors[4] = '#00FF00';
-    this.arrayColors[5] = '#004400';
+    this.arrayColors[2] = '#55b018';
+    this.arrayColors[3] = '#df0000';
+    this.arrayColors[4] = '#ff6d00';
+    this.arrayColors[5] = '#696969';
   }
 
   onColorSelected(idx: number) {
@@ -32,12 +32,10 @@ export class DrawingSurfaceToolComponent implements OnInit {
   }
 
   pickTextColorBasedOnBgColorSimple(bgColor: string): string {
-    console.log(bgColor);
     let color = (bgColor.charAt(0) === '#') ? bgColor.substring(1, 7) : bgColor;
     let r = parseInt(color.substring(0, 2), 16); // hexToR
     let g = parseInt(color.substring(2, 4), 16); // hexToG
     let b = parseInt(color.substring(4, 6), 16); // hexToB
-    console.log(r, g, b, (((r * 0.299) + (g * 0.587) + (b * 0.114)) > 186));
     return (((r * 0.299) + (g * 0.587) + (b * 0.114)) > 120) ? "#000000" : "#FFFFFF";
   }
 }
