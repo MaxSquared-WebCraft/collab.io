3defd5f4cdb918f850c1bc8fef6a290184f1973d
Maximilian Zauner <zaunermax@gmail.com>
2018-06-25 16:00:42 +0200

    refactor(code): refactored code

--- src/app/shared/services/websocket.service.ts
+++ src/app/shared/services/websocket.service.ts
@@ -1,6 +1,5 @@
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
-import { QueueingSubject } from 'queueing-subject';
 import { RoomService } from './room.service';
 import { connect } from 'socket.io-client';
 import { AuthService } from './auth.service';
@@ -13,10 +12,7 @@ import { environment } from '../../../environments/environment';
 @Injectable()
 export class SocketService {
   public messages: Observable<SocketMessage>;
-  private inputStream: QueueingSubject<string>;
-  private connectedClients: any[]; // should contain observable of connected users and show positions
   private socket: SocketIOClient.Socket = null;
-
 
   constructor(
     private readonly roomService: RoomService,
@@ -41,29 +37,6 @@ export class SocketService {
       this.socket.on('message', (message) => subscriber.next(message));
     })).pipe(share());
   }
-
-  /*
-  public connect() {
-    if (this.messages) {
-      return;
-    }
-
-    this.messages = websocketConnect('ws://127.0.0.1:1337/ws',
-      this.inputStream = new QueueingSubject<string>())
-      .messages
-      .pipe(
-        map((message: string) => {
-          try {
-            const msg: any = JSON.parse(message);
-            return ({ time: msg.data.time, ...JSON.parse(msg.data.utf8Data) });
-          } catch (e) {
-            return observableThrowError(e);
-          }
-        }),
-        share()
-      );
-  }
-  */
 
   public readonly send = (p: any): void => {
     this.socket.emit('message', p)
